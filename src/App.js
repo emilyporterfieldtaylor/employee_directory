2921048f3f9d1b951befeabc28f98de0f5996a4e
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-04-29 21:21:42 -0500

    create Header Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Header/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +13,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
