c2311160b62a7c70b9ade9a5d4272c0f43e16e7e
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-04-30 22:56:32 -0500

    Add styling to Header and TableHeader

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
+import "./style.css";
 
 function Header(props) {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
         <h1 className="display-4">Employee Directory</h1>
-        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        <input className="search" type="text" value={props.searchText} placeholder="Search for Employee" onChange={props.search} />
+        <input className="form-control" type="text" value={props.searchText} placeholder="Search for Employee" onChange={props.search} />
       </div>
     </div>
   )
--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './style.css';
 
 function TableHeader(props) {
   return (
   <thead>
     <tr>
       <th scope="col">Image</th>
-      <th scope="col" onClick={ () =>  props.toggleSort("first")}>First Name</th>
-      <th scope="col" onClick={ () =>  props.toggleSort("last")}>Last Name</th>
+      <th className="clickable" scope="col" onClick={ () =>  props.toggleSort("first")}>First Name</th>
+      <th className="clickable" scope="col" onClick={ () =>  props.toggleSort("last")}>Last Name</th>
       <th scope="col">Phone Number</th>
       <th scope="col">Email</th>
     </tr>
