bea3889471ee88d01432cb25c383d190498a3cf3
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-04-30 22:59:01 -0500

    change company name on header to big austin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h1 className="display-4">Employee Directory</h1>
+        <h1 className="display-4">Big Austin: Employee Directory</h1>
         <input className="form-control" type="text" value={props.searchText} placeholder="Search for Employee" onChange={props.search} />
       </div>
     </div>
